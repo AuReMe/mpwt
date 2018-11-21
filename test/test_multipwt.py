fdfcfd4452bad5f884bc250f604f86bb7d4ee89f
Arnaud Belcour <arnaud.belcour@irisa.fr>
2018-11-21 17:11:48 +0100

    Add another genbank file for the test.

--- test/test_multipwt.py
+++ test/test_multipwt.py
@@ -34,19 +34,30 @@ def test_create_dats_and_lisp():
 def test_multiprocess_pwt():
     mpwt.multiprocess_pwt('test', 'test_output', dat_extraction=True, size_reduction=False, verbose=True)
 
-    pathway_pathname = "test_output/tca_cycle_ecoli/pathways.dat"
+    pathway_tca_pathname = "test_output/tca_cycle_ecoli/pathways.dat"
 
     tca_reactions = ["RXN-14971", "MALATE-DEH-RXN", "ISOCITDEH-RXN", "MALATE-DEHYDROGENASE-ACCEPTOR-RXN",
                     "ACONITATEDEHYDR-RXN", "CITSYN-RXN", "ACONITATEHYDR-RXN", "2OXOGLUTARATEDEH-RXN", "SUCCCOASYN-RXN", "FUMHYDR-RXN"]
-    reactions = []
-    with open(pathway_pathname, 'r') as pathway_file:
+    expected_tca_reactions = []
+    with open(pathway_tca_pathname, 'r') as pathway_file:
         for line in pathway_file:
             if 'REACTION-LIST' in line:
                 reaction = line.split(' - ')[1].strip()
 
-                reactions.append(reaction)
+                expected_tca_reactions.append(reaction)
 
-    assert set(tca_reactions).issubset(set(reactions))
+    assert set(tca_reactions).issubset(set(expected_tca_reactions))
 
-test_create_dats_and_lisp()
-test_multiprocess_pwt()
+    pathway_fabo_pathname = "test_output/fatty_acid_beta_oxydation_I/pathways.dat"
+
+    fabo_reactions = ["ACYLCOASYN-RXN", "ACYLCOADEHYDROG-RXN", "ENOYL-COA-DELTA-ISOM-RXN", "ENOYL-COA-HYDRAT-RXN",
+                    "OHBUTYRYL-COA-EPIM-RXN", "OHACYL-COA-DEHYDROG-RXN", "KETOACYLCOATHIOL-RXN"]
+    expected_fabo_reactions = []
+    with open(pathway_fabo_pathname, 'r') as pathway_file:
+        for line in pathway_file:
+            if 'REACTION-LIST' in line:
+                reaction = line.split(' - ')[1].strip()
+
+                expected_fabo_reactions.append(reaction)
+
+    assert set(fabo_reactions).issubset(set(expected_fabo_reactions))
