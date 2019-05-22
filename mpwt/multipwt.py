8caea497a6a56fb825d9f9e62f9a6282bd3ed78c
Arnaud Belcour <arnaud.belcour@irisa.fr>
2019-05-22 10:44:45 +0200

    Fix issue with --clean, -v and --cpu (issue #20).

--- mpwt/multipwt.py
+++ mpwt/multipwt.py
@@ -906,7 +906,7 @@ def run_mpwt():
         utils.cleaning(number_cpu, verbose)
         if input_folder:
             utils.cleaning_input(input_folder, verbose)
-        if argument_number == 1 or (argument_number == 2 and verbose):
+        if argument_number == 1 or (argument_number == 2 and verbose) or (argument_number == 3 and verbose and number_cpu):
             sys.exit()
 
     multiprocess_pwt(input_folder,
