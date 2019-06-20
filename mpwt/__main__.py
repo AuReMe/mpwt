AuReMe/mpwt

commit d466fe81737a0ff25c6859d3e1487f797f3eb5c3

Modify --clean behaviour to delete PGDB from input folders (issue #23). Fix typos. Update Readme according to this change.

mpwt/__main__.py
@@ -83,9 +83,13 @@ def run_mpwt():
     if args['--clean']:
         if verbose:
             logger.info('~~~~~~~~~~Remove local PGDB~~~~~~~~~~')
-        utils.cleaning(number_cpu, verbose)
+
         if input_folder:
             utils.cleaning_input(input_folder, verbose)
+            input_pgdb_to_deletes = [species.lower() + 'cyc' for species in os.listdir(input_folder)]
+            utils.remove_pgbds(input_pgdb_to_deletes, number_cpu)
+        else:
+            utils.cleaning(number_cpu, verbose)
         if argument_number == 1 or (argument_number == 2 and verbose) or (argument_number == 4 and verbose and number_cpu):
             sys.exit()
 

mpwt/pwt_wrapper.py
@@ -170,7 +170,7 @@ def run_move_pgdb(move_data):
     pgdb_folder_path: path to the PGDB of the species (in ptools-local).
 
     Args:
-        move_data (dictionary): contains multiprocess input (PGDB ID, pttols-local PGDB pathname, ...)
+        move_data (dictionary): contains multiprocess input (PGDB ID, ptools-local PGDB pathname, ...)
     """
     pgdb_folder_dbname = move_data['pgdb_folders'][0]
     pgdb_folder_path = move_data['pgdb_folders'][1]

commit 610c8468c221f295b84dfefd83ef9c0cb3325db0

Fix possible issue introduced in d466fe81737a0ff25c6859d3e1487f797f3eb5c3. Rename pf_taxon_id.txt into taxon_id.tsv.

mpwt/__main__.py
@@ -86,7 +86,7 @@ def run_mpwt():
 
         if input_folder:
             utils.cleaning_input(input_folder, verbose)
-            input_pgdb_to_deletes = [species.lower() + 'cyc' for species in os.listdir(input_folder)]
+            input_pgdb_to_deletes = [species.lower() + 'cyc' for species in os.listdir(input_folder) if not species.startswith('.') and species != 'taxon_id.tsv']
             utils.remove_pgdbs(input_pgdb_to_deletes, number_cpu)
         else:
             utils.cleaning(number_cpu, verbose)

mpwt/pathologic_input.py
@@ -65,8 +65,8 @@ def check_input_and_existing_pgdb(run_ids, input_folder, output_folder, verbose=
         return None, None
 
     # Remove Pathologic taxon ID file.
-    if 'pf_taxon_id.txt' in species_folders:
-        species_folders.remove('pf_taxon_id.txt')
+    if 'taxon_id.tsv' in species_folders:
+        species_folders.remove('taxon_id.tsv')
 
     # Check if there is a Genbank, a GFF or a PathoLogic file inside each subfolder.
     input_extensions = ['.gbk', '.gff', '.pf']
@@ -239,12 +239,12 @@ def create_dats_and_lisp(run_folder):
                     raise FileNotFoundError('No fasta file with the Pathologic file of {0}'.format(pgdb_id))
 
                 input_folder =  os.path.abspath(os.path.join(run_folder ,os.pardir))
-                with open(input_folder + '/pf_taxon_id.txt') as pf_taxon_id:
+                with open(input_folder + '/taxon_id.tsv') as pf_taxon_id:
                     for line in pf_taxon_id.readlines():
                         if pgdb_id in line:
                             taxon_id = line.split('\t')[1]
         if not taxon_id:
-            raise Exception('Missing "taxon:" in Pathologic file of {0} in {1}.'.format(pgdb_id, input_folder + '/pf_taxon_id.txt'))
+            raise Exception('Missing "taxon:" in Pathologic file of {0} in {1}.'.format(pgdb_id, input_folder + '/taxon_id.tsv'))
 
     lisp_pathname = run_folder + "dat_creation.lisp"
 