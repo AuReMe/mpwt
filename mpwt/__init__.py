f2f63e4590fb6bf24e3aa607012c6cdec4baabcb
Arnaud Belcour <arnaud.belcour@inria.fr>
2021-02-12 17:26:27 +0100

    Add LGPL licence.

--- mpwt/__init__.py
+++ mpwt/__init__.py
@@ -1,3 +1,17 @@
+# Copyright (C) 2018-2021 Arnaud Belcour & Clémence Frioux - Inria Dyliss - Pleiade
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>
+
 from mpwt.pwt_wrapper import run_pwt, run_pwt_flat
 from mpwt.mpwt_workflow import multiprocess_pwt
 from mpwt.utils import cleaning, cleaning_input, find_ptools_path, list_pgdb, pubmed_citations, remove_pgdbs
